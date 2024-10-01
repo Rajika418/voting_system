1d78385514509cc269f7e69c44b2f3d2bdca8471
YathuPiraba <yathavan1407@gmail.com>
2024-10-01 07:46:43 +0530

    removed al results

--- client/admin/results/add_result.js
+++ client/admin/results/add_result.js
@@ -1,6 +1,9 @@
 const API_URL = 'http://localhost/voting_system/server/controller/subject/ol_subject_get.php?action=read';
 const RESULT_OPTIONS = ['A', 'B', 'C', 'S', 'W'];
 
+const API_URL1 = 'http://localhost/voting_system/server/controller/subject/al_subject_get.php?action=read';
+
+
 async function fetchSubjects() {
     try {
         const response = await axios.get(API_URL);
