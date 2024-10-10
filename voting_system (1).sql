dd0a52b2514391836e738734eeba363302cf4736
Rajika418 <rajikakumar18@gmail.com>
2024-10-10 04:11:32 -0700

    change the css

--- voting_system (1).sql
+++ voting_system (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 02, 2024 at 12:56 AM
+-- Generation Time: Oct 10, 2024 at 01:09 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -119,25 +119,11 @@ INSERT INTO `grade` (`grade_id`, `grade_name`, `teacher_id`, `year`) VALUES
 (47, '11A', NULL, 11),
 (48, '11B', NULL, 11),
 (49, '11C', NULL, 11),
-(50, '11D', NULL, 11);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `grade_teacher`
---
-
-CREATE TABLE `grade_teacher` (
-  `grade_id` int(11) NOT NULL,
-  `teacher_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `grade_teacher`
---
-
-INSERT INTO `grade_teacher` (`grade_id`, `teacher_id`) VALUES
-(18, 15);
+(50, '11D', NULL, 11),
+(51, '12A', NULL, 12),
+(52, '13A', NULL, 13),
+(53, '13B', NULL, 13),
+(54, '13C', NULL, 13);
 
 -- --------------------------------------------------------
 
@@ -168,15 +154,29 @@ CREATE TABLE `results` (
 --
 
 INSERT INTO `results` (`result_id`, `subject_id`, `result`, `exam_id`) VALUES
-(10, 1, 'A', 2),
-(11, 2, 'A', 2),
-(12, 30, 'A', 2),
-(13, 31, 'A', 2),
-(14, 3, 'A', 2),
-(15, 8, 'S', 2),
-(16, 10, 'A', 2),
-(17, 16, 'A', 2),
-(18, 26, 'C', 2);
+(19, 1, 'A', 12),
+(20, 2, 'A', 12),
+(21, 30, 'A', 12),
+(22, 31, 'A', 12),
+(23, 4, 'A', 12),
+(24, 8, 'A', 12),
+(25, 11, 'A', 12),
+(26, 15, 'A', 12),
+(34, 46, 'A', 14),
+(35, 47, 'A', 14),
+(36, 48, 'A', 14),
+(37, 35, 'A', 14),
+(38, 36, 'A', 14),
+(39, 45, 'C', 14),
+(40, 1, 'C', 15),
+(41, 2, 'A', 15),
+(42, 30, 'B', 15),
+(43, 31, 'B', 15),
+(44, 4, 'A', 15),
+(45, 8, 'A', 15),
+(46, 11, 'A', 15),
+(47, 15, 'A', 15),
+(48, 29, 'A', 15);
 
 -- --------------------------------------------------------
 
@@ -249,21 +249,10 @@ CREATE TABLE `student` (
 --
 
 INSERT INTO `student` (`student_id`, `student_name`, `father_name`, `user_id`, `grade_id`, `address`, `guardian`, `contact_number`, `registration_number`, `join_date`, `leave_date`) VALUES
-(5, 'k.Doe join', 'null', 31, 47, '123 Main Street', 'Jane Doe', '555-1234', '2024001', '0000-00-00', NULL),
-(6, 'k.Doe jone', 'null', 32, 48, '123 Main Street', 'Jane Doe', '555-1234', '2024001', '0000-00-00', NULL),
-(7, 'T.Santhosh', NULL, 34, 22, 'bungalow divition hopton', 'Thiruselvam', '0775248369', 'RGE12458', '2011-10-18', NULL),
-(8, 'K.DAYANI', NULL, 35, 21, 'bungalow divition hopton', 'Thiruselvagogam', '0775248369', 'RGE12453', '2023-08-10', NULL),
-(9, 'rajikakumar', NULL, 50, 8, '123 Main Street, Springfield', 'ane Doe', '1234567890', 'REG123459', '2024-09-06', NULL),
-(11, 'kavipriyadharsini', NULL, 52, 10, '123 Main Street, Springfield', 'kamaraj', '1234567888', 'REG123469', '2024-09-06', NULL),
-(12, 'gjhjk', NULL, 53, 14, 'nkjnjn', 'fgj', '0775248369', 'RGE12458', '2024-09-12', NULL),
-(13, 'nadee', NULL, 54, 20, 'nkjnjn', 'fgj', '0775248369', 'RGE12458', '2024-09-12', NULL),
-(14, 'nadee', NULL, 55, 20, 'nkjnjn', 'fgj', '0775248369', 'RGE12458', '2024-09-12', NULL),
-(15, 'nadeesha', NULL, 56, 17, 'bungalow divition hopton', 'Thiruselvam', '0775248369', 'RGE12453', '2024-09-14', NULL),
-(16, 'nivitha', NULL, 57, 10, '123 Main Street, Springfield', 'kamaraj', '1234567888', 'REG123469', '2024-09-06', NULL),
-(17, 'nivithaya', 'kamal', 58, 10, '123 Main Street, Springfield', NULL, '1234567888', 'REG123463', '2024-09-06', NULL),
-(18, 'nivithaya', 'y.kamal', 59, 10, '123 Main Street, Springfield', 'null', '1234567888', 'REG123463', '0000-00-00', NULL),
-(19, 'nivithaya', 'kamal', 60, 10, '123 Main Street, Springfield', NULL, '1234567888', 'REG123463', '2024-09-06', NULL),
-(20, 'gjhjk', 'ewstryh', 61, 7, 'nkjnjn', NULL, '0775248369', 'RGE12453', '2024-09-13', NULL);
+(21, 'nivithaya', 'kamal', 62, 52, '123 Main Street, Springfield', 'null', '1234567888', 'REG123463', '2024-09-06', NULL),
+(22, 'kamalanathan', 'kamal', 63, 53, '123 Main Street, Springfield', NULL, '1234567889', 'REG123464', '2024-09-06', NULL),
+(23, 'mary', 'Antony', 64, 49, '123 Main Street, Springfield', 'null', '1234567810', 'REG123465', '2024-09-06', NULL),
+(24, 'Dthurai', 'Dhurai', 65, 50, '123 Main Street, Springfield', NULL, '1234567811', 'REG123467', '2024-09-06', NULL);
 
 -- --------------------------------------------------------
 
@@ -285,9 +274,9 @@ CREATE TABLE `student_exam` (
 --
 
 INSERT INTO `student_exam` (`id`, `student_id`, `exam_name`, `year`, `index_no`, `nic`) VALUES
-(2, 11, 'GCE O/L', '2024', '123456', '123456789V'),
-(10, 5, 'ol', '2024', '5678', '123456789789'),
-(11, 6, 'General Certificate of Education - Ordinary Level (G.C.E(O/L))', '2024', '4562', '123456789789');
+(12, 24, 'General Certificate of Education - Ordinary Level (G.C.E(O/L))', '2024', '12345', '123456789'),
+(14, 21, 'General Certificate of Education - Advanced Level (G.C.E(A/L))', '2024', '5461', '7894562'),
+(15, 23, 'General Certificate of Education - Ordinary Level (G.C.E(O/L))', '2024', '3698', '7894562');
 
 -- --------------------------------------------------------
 
@@ -314,43 +303,31 @@ INSERT INTO `subjects` (`subject_id`, `year`, `section`, `subject_name`) VALUES
 (5, 'o/l', 'Religions', 'Catholicism'),
 (6, 'o/l', 'Religions', 'Christianily'),
 (7, 'o/l', 'Religions', 'Islam'),
-(8, 'o/l', 'Language', 'Tamil Language & Literature'),
-(9, 'o/l', 'Language', 'Sinhala Language & Literature'),
+(8, 'o/l', '', 'Tamil Language & Literature'),
 (10, 'o/l', '1st Subject Group', 'Business & Accounting Studies'),
 (11, 'o/l', '1st Subject Group', 'Geography'),
-(12, 'o/l', '1st Subject Group', 'Second Language (Tamil)'),
 (13, 'o/l', '1st Subject Group', 'Second Language (Sinhala)'),
 (14, 'o/l', '1st Subject Group', 'Civic Education'),
 (15, 'o/l', '2nd Subject Group', 'Music (Carnatic)'),
 (16, 'o/l', '2nd Subject Group', 'Art'),
-(17, 'o/l', '2nd Subject Group', 'Dancing (Bharatha)'),
-(18, 'o/l', '2nd Subject Group', 'English Literature '),
 (19, 'o/l', '2nd Subject Group', 'Tamil Literature'),
-(20, 'o/l', '2nd Subject Group', 'Drama & Theatre'),
-(21, 'o/l', '2nd Subject Group', 'Sinhala Literature'),
 (22, 'o/l', '3rd Subject Group', 'Information & Communication Technology'),
 (23, 'o/l', '3rd Subject Group', 'Aquatic Bioresources Technology'),
-(24, 'o/l', '3rd Subject Group', 'Arts & Crafts'),
 (25, 'o/l', '3rd Subject Group', 'Home Economics'),
 (26, 'o/l', '3rd Subject Group', 'Health & Physical Education'),
-(27, 'o/l', '3rd Subject Group', 'Communication & Media Studies'),
-(28, 'o/l', '3rd Subject Group', 'Electrical & Electronic Technology'),
 (29, 'o/l', '3rd Subject Group', 'Agriculture & Food Technology'),
 (30, 'o/l', NULL, 'History'),
 (31, 'o/l', NULL, 'Science'),
-(33, 'A/L', 'Arts', 'Tamil'),
-(34, 'A/L', 'Arts', 'Tamil'),
-(35, 'A/L', 'Arts', 'Tami(A/L)'),
-(36, 'A/L', 'Arts', 'Political Sceince'),
-(37, 'A/L', 'Arts', 'Logic & Scientific Method'),
-(38, 'A/L', 'Arts', 'Hindu Civilization'),
-(39, 'A/L', 'Arts', 'Christian Civilization'),
-(40, 'A/L', 'Arts', 'Islam Civilization'),
-(41, 'A/L', 'Arts', 'English(A/L)'),
-(42, 'A/L', 'Arts', 'Carnatic Music(A/L)'),
-(43, 'A/L', 'Arts', 'Dancing (Bharatha)(A/L)'),
-(44, 'A/L', 'Arts', 'History(A/L)'),
-(45, 'A/L', 'Arts', 'Geography(A/L)'),
+(35, 'A/L', 'Subject 1', 'Tami(A/L)'),
+(36, 'A/L', 'Subject 1', 'Political Sceince'),
+(37, 'A/L', 'Subject 1', 'Logic & Scientific Method'),
+(38, 'A/L', 'Subject 1', 'Hindu Civilization'),
+(39, 'A/L', 'Subject 1', 'Christian Civilization'),
+(40, 'A/L', 'Subject 1', 'Islam Civilization'),
+(42, 'A/L', 'Subject 1', 'Carnatic Music(A/L)'),
+(43, 'A/L', 'Subject 1', 'Dancing (Bharatha)(A/L)'),
+(44, 'A/L', 'Subject 1', 'History(A/L)'),
+(45, 'A/L', 'Subject 1', 'Geography(A/L)'),
 (46, 'A/L', NULL, 'General English'),
 (47, 'A/L', NULL, 'General Knowladage'),
 (48, 'A/L', NULL, 'GIT');
@@ -366,18 +343,6 @@ CREATE TABLE `subject_teacher` (
   `subject_id` int(11) DEFAULT NULL,
   `teacher_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `subject_teacher`
---
-
-INSERT INTO `subject_teacher` (`subject_teacher_id`, `subject_id`, `teacher_id`) VALUES
-(1, 2, 1),
-(2, 2, 2),
-(14, 1, 16),
-(15, 1, 17),
-(16, 1, 18),
-(24, 1, 27);
 
 -- --------------------------------------------------------
 
@@ -402,15 +367,7 @@ CREATE TABLE `teacher` (
 
 INSERT INTO `teacher` (`teacher_id`, `teacher_name`, `address`, `contact_number`, `nic`, `user_id`, `join_date`, `leave_date`) VALUES
 (1, 'Anusha', '123 Main Street, City, Colombo', '071234567', '987654321V', 12, '0000-00-00', '0000-00-00'),
-(2, 'Pragash', ' Main Street, badulla, Sri Lanka', '0712345678', '200058426491', 13, NULL, NULL),
-(15, 'K.nilila', '456 Another Street, badulla, srilanka', '0723456785', '123456799V', 37, '2024-09-01', NULL),
-(16, 'K.nilila', '456 Another Street, badulla, srilanka', '0723456785', '123456799V', 38, '2024-09-01', NULL),
-(17, 'K.nilila', '456 Another Street, badulla, srilanka', '0723456785', '123456799V', 39, '2024-09-01', NULL),
-(18, 'K.nilila', '456 Another Street, badulla, srilanka', '0723456785', '123456799V', 40, '2024-09-01', NULL),
-(19, 'K.nilila', '456 Another Street, badulla, srilanka', '0723456785', '123456799V', 41, '2024-09-01', NULL),
-(20, 'K.nilila', '456 Another Street, badulla, srilanka', '0723456785', '123456799V', 42, '2024-09-01', NULL),
-(26, 'rioshiniya', 'bungalow divition hopton', '0775248369', '123456789789', 48, '2024-09-05', NULL),
-(27, 'niro', 'bungalow divition hopton', '0775248369', '12335121', 49, '2024-09-04', '0000-00-00');
+(2, 'Pragash', ' Main Street, badulla, Sri Lanka', '0712345678', '200058426491', 13, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -438,10 +395,6 @@ INSERT INTO `users` (`user_id`, `user_name`, `password`, `email`, `role_id`, `im
 (11, 'student', '$2y$10$RgS2NmBonL.cXJ1ly/wmkOLTrx1VfViQXNKkqxhhmDGy2RTJIFC4K', 'student@example.com', 3, NULL),
 (12, 'anusha', '$2y$10$76dHSMUt/7cNnYVtgQF53uh7krWDKi9hSXaX1U7ipLlR.5CcWZEbe', 'anusha@example.com', 2, NULL),
 (13, 'pre', '$2y$10$3HiPsuOkwT5485KN3F53fezwNjGBq6jpCzVnjSECGdwihceJ1gGsW', 'pre@example.com', 2, NULL),
-(31, 'johndoe123', '$2y$10$L77.TDnbJ6s2.MRAQIensOIkU9L9pAbs.oqt.Ctt1Out4y4B3TG/W', 'johndoe@example.com', 3, 0x7363686f6f6c2e6a706567),
-(32, 'johndoe123', '$2y$10$hEw5kfQo21SEFnBskns6e.2sIPlgPk4Lb6G7UdvT/nZvaDNznZwJK', 'johndoe@example.com', 3, 0x7363686f6f6c2e6a706567),
-(34, 'santhosh', '$2y$10$8rCd7NnfvsSW.khZSXfw8eP602C3vXLfSLtM1eyDsbKITJmKGk7c2', 'santhosg@gmail.com', 3, 0x746561636865722e706e67),
-(35, 'daya', '$2y$10$PYKQa68QC2YD3JZJfp9HB.fnCU37RkQuv3fZMmqOlOsuf/LTKhuDC', 'daya@gmail.com', 3, 0x7374752e706e67),
 (36, 'divishi', '$2y$10$ziveYIAu8LKYitlT6ddtcuIGndjdKIaFbc5A286wbgcaWfPy8zf3W', 'divishi@example.com', 2, NULL),
 (37, 'nimi', '$2y$10$ZKqjHE6JPm03kWq4vw8IWeRjunokICUdemUADNNXwZYOB/2lmChhu', 'nimi@example.com', 2, NULL),
 (38, 'nimi', '$2y$10$T72pfhoRTz6zT9LxJs1NuewgkYUKYrnZEMKMMffswf6Oa.M3haEUO', 'nimi@example.com', 2, NULL),
@@ -449,20 +402,11 @@ INSERT INTO `users` (`user_id`, `user_name`, `password`, `email`, `role_id`, `im
 (40, 'nimi', '$2y$10$xCLwTnfqQPieXUwtTZ7RCO4ny2sba4FJYLu2G/JMi/XiBb0XR3/Hi', 'nimi@example.com', 2, NULL),
 (41, 'nimi', '$2y$10$tUjJDQreur2nSd0EGlKFUukEUJgCZ8OYEvdjX1.I4U3Ndh9w77N1O', 'nimi@example.com', 2, NULL),
 (42, 'nimi', '$2y$10$xyth5JnySE9.PXu9W3BuieYoZuQIOGy./xDd5a5w.jjzEEd6aOR9m', 'nimi@example.com', 2, NULL),
-(48, 'abcd', '$2y$10$Nc3lX54tCNl147m5pLuMqei4hr48yOpIEJFEWdHzw5nYYudqcdKpm', 'santhosg@gmail.com', 2, 0x687474703a2f2f6c6f63616c686f73742f766f74696e675f73797374656d2f75706c6f6164732f363664653733613633313538635f51505049303933382e4a5047),
-(49, 'abcv', '$2y$10$oWiWKMNaD7Ok6w3Zsc7cPeLLjMuG9blpG74R.8cd4mpsJh64ZRdL6', 'rajikakumar18@gmail.com', 2, 0x687474703a2f2f6c6f63616c686f73742f766f74696e675f73797374656d2f75706c6f6164732f363664653734363336636637385f52484b41313530382e4d4f56),
-(50, 'kuma', '$2y$10$KIXjSZTEM4eJZ9KDyEHJLOBK.3Vu5k8wy4kmFAPud7lEV4UMedGsS', 'kumar@example.com', 3, 0x61646d696e2074652e706e67),
 (51, '', '$2y$10$DTmIbTpKnVoCOScNOYj8AuPOGdZKVQmIQgsjdjyB3Kvu10lihsDkS', '', 3, NULL),
-(52, 'papa', '$2y$10$nsQSeqrEzMJD78oLNrxDJuTqhE0V5ICdb7jmNwmD6ZGHrl44NF.pi', 'papa@example.com', 3, 0x687474703a2f2f6c6f63616c686f73742f766f74696e675f73797374656d2f75706c6f6164732f363665396665653032663434395f61646d696e2e706e67),
-(53, 'fgh', '$2y$10$TPSsFhnM68XosM3v/BbsoeKh0cf3cNrhw02hp.EkQPn776FZU8y6a', 'santhosg@gmail.com', 3, 0x687474703a2f2f6c6f63616c686f73742f766f74696e675f73797374656d2f75706c6f6164732f363666386162653863616637305f415349474e2e706e67),
-(54, 'fgh', '$2y$10$Xh7fIOGZdIgMVB4xnRSxleXV5q7tX2x1SVMRODoNTRybLYPcptKle', 'santhosg@gmail.com', 3, 0x687474703a2f2f6c6f63616c686f73742f766f74696e675f73797374656d2f75706c6f6164732f363666386164346136666637355f415349474e2e706e67),
-(55, 'fgh', '$2y$10$7s8ppLrJDoz3SuicOepYleHKoQlJTBatD5GmS16MgtXgLwPA4VLeG', 'santhosg@gmail.com', 3, 0x687474703a2f2f6c6f63616c686f73742f766f74696e675f73797374656d2f75706c6f6164732f363666386164346232363830615f415349474e2e706e67),
-(56, 'lkj', '$2y$10$5Q4O0YMWtfssA4NqUh4TyeXA6SSYbZGjAjFGue0yG9j9zK30rDQg2', 'daya@gmail.com', 3, 0x687474703a2f2f6c6f63616c686f73742f766f74696e675f73797374656d2f75706c6f6164732f363666393366326532633664615f61646d696e2e706e67),
-(57, 'nivi', '$2y$10$LL0Ce6yt/GUZb1E9duSwT.58X.i.7k8kh26rrKn9.p2dhT1Hcb9ry', 'papa@example.com', 3, 0x687474703a2f2f6c6f63616c686f73742f766f74696e675f73797374656d2f75706c6f6164732f363666393430633131623836305f61646d696e312e706e67),
-(58, 'niviya', '$2y$10$/mZe6ynL5YFGjmX4ntY9zOedMc6RnulQN5e4ZnsX.oejjkBh/0R6C', 'nivi@example.com', 3, 0x687474703a2f2f6c6f63616c686f73742f766f74696e675f73797374656d2f75706c6f6164732f363666393436303163316333375f61646d696e312e706e67),
-(59, 'niviya', '$2y$10$nx2NlhvoqgyKwtKRy4S/RORElLx2fMerxG3aDYV2aN2xvQ/c.m20O', 'nivi@example.com', 3, 0x687474703a2f2f6c6f63616c686f73742f766f74696e675f73797374656d2f75706c6f6164732f363666393436363032343866655f61646d696e312e706e67),
-(60, 'niviya', '$2y$10$jsmSyb5hSX2XMZgw2fWguuccac7bdAq61mI4efgNIH9aJo/.37C/K', 'nivi@example.com', 3, 0x687474703a2f2f6c6f63616c686f73742f766f74696e675f73797374656d2f75706c6f6164732f363666393436386534633337375f61646d696e312e706e67),
-(61, 'fgh', '$2y$10$8NK/cbK03CFgq.WseaIuauVLyrxRluNifZU/KoCBxSIeKcXpXYkWG', 'santhosg@gmail.com', 3, 0x687474703a2f2f6c6f63616c686f73742f766f74696e675f73797374656d2f75706c6f6164732f363666393437396134663633365f6f6c2e706e67);
+(62, 'niviya', '$2y$10$CFnsSZiB/2R98daJpIgprOQbqcc9jurTAx6chvklOpZuBuN3itX6m', 'nivi@example.com', 3, 0x687474703a2f2f6c6f63616c686f73742f766f74696e675f73797374656d2f75706c6f6164732f363666643733353939653565325f494d475f3036323220636f70792e6a7067),
+(63, 'kamal', '$2y$10$fnEdCmoRU8Z7OIAr3g30vO4koLsKGjA4.L5NKW/GIZgr7ZptXJolK', 'kamal@example.com', 3, 0x687474703a2f2f6c6f63616c686f73742f766f74696e675f73797374656d2f75706c6f6164732f363666643733636632656439615f4d4a484d303937312e4a5047),
+(64, 'mary', '$2y$10$//cSAYv4by27m/ECuhTZz.cB/AlV.GLTpqfMH5u6O55lzYZsxdW1q', 'mary@example.com', 3, 0x687474703a2f2f6c6f63616c686f73742f766f74696e675f73797374656d2f75706c6f6164732f363666643734623637633034315f4443544c393635382e4a5047),
+(65, 'dhurai', '$2y$10$aLQ6PT.UtcfOsYNaHRnUJ.6hSFD80acTK4B0w4zseqEj4bLeCI.zu', 'dhurai@example.com', 3, 0x687474703a2f2f6c6f63616c686f73742f766f74696e675f73797374656d2f75706c6f6164732f363666643735316264623937365f4443544c393635382e4a5047);
 
 --
 -- Indexes for dumped tables
@@ -494,13 +438,6 @@ ALTER TABLE `election_candidate`
 --
 ALTER TABLE `grade`
   ADD PRIMARY KEY (`grade_id`),
-  ADD KEY `teacher_id` (`teacher_id`);
-
---
--- Indexes for table `grade_teacher`
---
-ALTER TABLE `grade_teacher`
-  ADD PRIMARY KEY (`grade_id`,`teacher_id`),
   ADD KEY `teacher_id` (`teacher_id`);
 
 --
@@ -600,7 +537,7 @@ ALTER TABLE `election_candidate`
 -- AUTO_INCREMENT for table `grade`
 --
 ALTER TABLE `grade`
-  MODIFY `grade_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+  MODIFY `grade_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 
 --
 -- AUTO_INCREMENT for table `nomination`
@@ -612,7 +549,7 @@ ALTER TABLE `nomination`
 -- AUTO_INCREMENT for table `results`
 --
 ALTER TABLE `results`
-  MODIFY `result_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `result_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 
 --
 -- AUTO_INCREMENT for table `roles`
@@ -630,19 +567,19 @@ ALTER TABLE `staffs`
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `student_exam`
 --
 ALTER TABLE `student_exam`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `subjects`
 --
 ALTER TABLE `subjects`
-  MODIFY `subject_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+  MODIFY `subject_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
 
 --
 -- AUTO_INCREMENT for table `subject_teacher`
@@ -660,7 +597,7 @@ ALTER TABLE `teacher`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
 
 --
 -- Constraints for dumped tables
@@ -684,13 +621,6 @@ ALTER TABLE `election_candidate`
 --
 ALTER TABLE `grade`
   ADD CONSTRAINT `grade_ibfk_1` FOREIGN KEY (`teacher_id`) REFERENCES `teacher` (`teacher_id`);
-
---
--- Constraints for table `grade_teacher`
---
-ALTER TABLE `grade_teacher`
-  ADD CONSTRAINT `grade_teacher_ibfk_1` FOREIGN KEY (`grade_id`) REFERENCES `grade` (`grade_id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `grade_teacher_ibfk_2` FOREIGN KEY (`teacher_id`) REFERENCES `teacher` (`teacher_id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `nomination`
